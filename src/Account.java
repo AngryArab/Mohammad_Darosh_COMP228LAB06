f5b0c5cf41ce563b6c8fe4234970ace8bc264efc
Mohammad <mdsalam1994@hotmail.com>
2022-12-07 03:49:33 -0500

    Account class started

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+
+
+    public static void Deposit(){
+
+        System.out.println("$500 has been deposited");
+
+    }
+
+    public static void Withdraw(){
+
+
+        System.out.println("$250 has been withdrawn");
+
+    }
+
+
+
+
+}
